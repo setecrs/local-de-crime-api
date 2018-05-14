a6448e87f066f5bfa0ef7359d979fefc741fa87d
Gabriel Franzoni <gabriel.franzoni@acad.pucrs.br>
2018-05-14 16:37:52 -0300

    api - responsavel initial commit
    criado a parte que irá interagir com comandos da tela de responsáveis (patch)

--- routes/responsavel_local.js
+++ routes/responsavel_local.js
@@ -5,9 +5,32 @@ const checkToken = require('../config/check_token');
 const express = require('express');
 const responsavelLocalRouter = express.Router();
 
-//patch
+//get e post não necessários pois são partes da Ocorrência.
+
+//patch (atualiza dados de entrada no Responsável)
+//parâmetro de entrada; id da ocorrência que queremos atualizar
 responsavelLocalRouter.patch('/:idOcorrencia', function(req, res) {
-    res.status(403).json({message: 'Operção ainda não suportada para /responsavel_local/:idOcorrencia'});
+        if (user = checkToken(req)) {
+        Ocorrencia.findOneAndUpdate({
+            _id: req.params.idOcorrencia,
+            criadoPor: user.id
+        }, {
+            nomeResponsavel: req.body.nomeResponsavel,
+            cargoResponsavel: req.body.cargoResponsavel,
+            documentoResponsavel: req.body.documentoResponsavel,
+            entrevistaResponsavel: req.body.entrevistaResponsavel,
+        }, {
+            new: true,
+            select: 'nomeResponsavel cargoResponsavel documentoResponsavel entrevistaResponsavel'
+        }, function(err, ocorrencia) {
+            if (err) res.status(500).json(err);
+            res.json(ocorrencia);
+        });
+    } else {
+        res.status(401).json({
+            message: 'Token inválido'
+        });
+    };
 });
 
 //router export
